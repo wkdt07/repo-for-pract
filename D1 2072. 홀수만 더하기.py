56b9b2105b3255c625d7641f04a0bacedf643a32
서현범 <shb1998@naver.com>
2024-01-12 11:25:44 +0900

    second

---
+++ D1 2072. 홀수만 더하기.py
@@ -0,0 +1,22 @@
+T=int(input())
+
+for t in range(1,T+1):
+    a=list(map(int,input().split()))
+    print(a)
+    for i in a:
+        if i % 2 == 0:
+            a.remove(i)
+    print(a)
+    total = sum(a)
+    print(f'#{t} {total}')
+
+
+# for t in range(1,T+1):
+#     a=list(map(int,input().split()))
+#     b=[]
+#     for i in a:
+#         if i % 2 != 0:
+#             b.append(i)
+#     print(b)
+#     total = sum(b)
+#     print(f'#{t} {total}')
---
+++ D2 1859 백만장자.py
@@ -0,0 +1,6 @@
+T=int(input())
+
+for t in range(1,T+1):
+    n=int(input())
+    price=list(map(int,input().split(' ')))
+    print(price)
